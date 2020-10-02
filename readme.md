3a40f0fd4bf1684bcbc2b636a8971e5ae1758129
ggomjae <ggomjae@naver.com>
2020-10-02 22:03:54 +0900

    인터페이스 기본 메소드와 스태틱 메소드2 Study
    Five - BarInterface, FooInterface, GomInterface
    App,Foo2

--- readme.md
+++ readme.md
@@ -7,6 +7,7 @@ Java 8 individual Study
 ### Study Description  
   
 > 2020-10-01 : 함수형 인터페이스와 람다 - One,Two,Three,Four -> Foo Class 
+> 2020-10-02 : 인터페이스 기본 메소드와 스태틱 메소드 - App, Foo2, five
 
 함수형 인터페이스와 람다 표현식<br>
 자바에서 제공하는 함수형 인터페이스<br>
--- src/main/java/me/ggomjae/java8to11/App.java
+++ src/main/java/me/ggomjae/java8to11/App.java
@@ -2,6 +2,12 @@ package me.ggomjae.java8to11;
 
 import me.ggomjae.java8to11.five.FooInterface;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Spliterator;
+
+
 public class App {
     public static void main(String[] args){
         Foo2 foo2 = new Foo2("ggomjae");
@@ -10,5 +16,50 @@ public class App {
 
         // static을 이용하여 사용가능.
         FooInterface.printAnything();
+
+        ////////////////////////////////////////////////////////
+        //       Six                                          //
+        ////////////////////////////////////////////////////////
+
+        List<String> name = new ArrayList<>();
+        name.add("ggomjae");
+        name.add("kyugnjae");
+        name.add("gomjae");
+
+
+        /*
+        forEach ->
+        Consumer는 어떤 객체를 입력받아 void를 출력시키는 함수형 인터페이스
+        이걸 보면 메소드 레퍼런스가 생각나야한다.
+        */
+        name.forEach( s->{
+            System.out.println(s);
+        });
+
+        /* 메소드 레퍼런스 ClassName::MethodName */
+        name.forEach(System.out::println);
+
+        /* tryAdvance 또한 Consumer를 받는다. */
+        Spliterator<String> spliterator = name.spliterator();
+        Spliterator<String> spliterator1 = spliterator.trySplit();
+        while(spliterator.tryAdvance(System.out::println));
+        System.out.println("========================");
+        while(spliterator1.tryAdvance((System.out::println)));
+
+        /* Stream 맛보기 */
+        long g = name.stream().map(String::toUpperCase)
+                .filter( s -> s.startsWith("g"))
+                .count();
+        System.out.println(g);
+
+        /* 삭제 */
+        name.removeIf(s->s.startsWith("g"));
+
+        /* 차순 , reversed를 이용한 역순*/
+        Comparator<String> compareToIgnoreCase = String::compareToIgnoreCase;
+        name.sort(compareToIgnoreCase);
+        name.sort(compareToIgnoreCase.reversed());
+
+
     }
 }
